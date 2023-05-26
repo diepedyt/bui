5808d503f2b5a2672e34d005cb1e5df7cb1a5b1a
diepedyt <106111219+diepedyt@users.noreply.github.com>
2023-05-26 21:19:51 +0100

    Create freeScriptLoader.lua

---
+++ freeScriptLoader.lua
@@ -0,0 +1,13 @@
+repeat task.wait() until game:IsLoaded()
+
+_G.DiscordInvite = "https://discord.gg/A33fTdna63"
+
+_G.Scripts = {
+    [8425128940] = "https://scripts.luawl.com/hosted/3423/19726/AnimeWarriors2Free.lua",
+    [8720980067] = "https://scripts.luawl.com/hosted/3423/19690/AnimeTalesScript.lua"
+}
+
+local s, f = pcall(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/diepedyt/bui/main/mainBuiLoaderNew.lua"))() end)
+
+repeat task.wait() until f or _G.Complete
+loadstring(game:HttpGet(_G.Scripts[game.PlaceId]))()
