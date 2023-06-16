e38da308f1cb6566a58b9f24a6f40aaca5b532c2
diepedyt <106111219+diepedyt@users.noreply.github.com>
2023-06-17 00:41:07 +0100

    Create temporynewsystem.lua

---
+++ temporynewsystem.lua
@@ -0,0 +1,26 @@
+repeat task.wait() until game:IsLoaded()
+
+local ui = loadstring(game:HttpGet('https://raw.githubusercontent.com/diepedyt/customLua/main/SimpleUiLib.lua'))()
+
+_G.KeyInserted = ui.CreateKeySystem((_G.Scripts.o:reverse()):lower(), "bstlar.com/1r/Fia")
+
+if _G.KeyInserted then
+    local main = ui.CreateMain()
+    for i,v in pairs(_G.Scripts) do
+        if type(v) == "table" then
+            local gameName = tostring(i)
+            pcall(function()
+                gameName = game:GetService("MarketplaceService"):GetProductInfo(i).Name
+            end)
+            ui.CreateSection(main, gameName)
+            for i,v in pairs(v) do
+                ui.CreateButton(main, i ,function()
+                        task.spawn(function()
+                                            loadstring(game:HttpGet(v))()
+                                end)
+                                            main:Destroy()
+                end)
+            end
+        end
+    end
+end
