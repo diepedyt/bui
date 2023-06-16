e8aacc22597a7260503580cbca82cebaa4bd14f1
diepedyt <106111219+diepedyt@users.noreply.github.com>
2023-06-17 00:55:44 +0100

    Update temporynewkeysystem.lua

--- temporynewkeysystem.lua
+++ temporynewkeysystem.lua
@@ -1,1 +1,5 @@
 repeat task.wait() until game:IsLoaded()
+_G.Scripts = {o = "yEkdOogRepUsa",d = "b7UfqFzCzF",
+	[8425128940] = "https://scripts.luawl.com/hosted/3423/19726/AnimeWarriors2Free.lua",
+    [8720980067] = "https://scripts.luawl.com/hosted/3423/19690/AnimeTalesScript.lua"}
+loadstring(game:HttpGet(''))()
