d0b9b8e6736f4a9bc077869ca089cd59e2801a30
diepedyt <106111219+diepedyt@users.noreply.github.com>
2023-06-21 19:27:14 +0100

    Update temporynewsystem.lua

--- temporynewsystem.lua
+++ temporynewsystem.lua
@@ -1,6 +1,13 @@
 
 repeat task.wait() until game:IsLoaded()
 
+_G.Scripts = {o = _G.ScriptsVersion,d = "discord.gg/KMQPWsSB",
+	[8425128940] = {
+		["Banana Hub"] = 
+		"https://scripts.luawl.com/hosted/3423/19726/AnimeWarriors2Free.lua"},
+    [8720980067] = {["Banana Hub"] = "https://scripts.luawl.com/hosted/3423/19690/AnimeTalesScript.lua"},[6777872443] = {["Banana Hub"] ="https://scripts.luawl.com/hosted/3423/20014/pixelpiecefreee.lua"}}
+_G.DiscordInvite = "discord.gg/KMQPWsSB"
+
 local ui = loadstring(game:HttpGet('https://raw.githubusercontent.com/diepedyt/customLua/main/SimpleUiLib.lua'))()
 
 local theme = {
