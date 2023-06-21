bd134bcb817ef3e6df53414fde16dc8caacf8584
diepedyt <106111219+diepedyt@users.noreply.github.com>
2023-06-21 19:29:47 +0100

    Update temporynewkeysystem.lua

--- temporynewkeysystem.lua
+++ temporynewkeysystem.lua
@@ -1,4 +1,4 @@
 repeat task.wait() until game:IsLoaded()
-_G.ScriptsVersion = "yEkdOogRepUsa"
+_G.ScriptsVersion = "yeKdoOgAhcus"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/diepedyt/bui/main/mainBuiLoaderNew.lua"))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/diepedyt/bui/main/temporynewsystem.lua'))()
