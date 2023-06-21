b313338e542fa8e5fae4fc40ce8e178c2228a68a
diepedyt <106111219+diepedyt@users.noreply.github.com>
2023-06-21 19:26:32 +0100

    Update temporynewkeysystem.lua

--- temporynewkeysystem.lua
+++ temporynewkeysystem.lua
@@ -1,5 +1,6 @@
 repeat task.wait() until game:IsLoaded()
-_G.Scripts = {o = "yEkdOogRepUsa",d = "discord.gg/KMQPWsSB",
+_G.ScriptsVersion = "yEkdOogRepUsa"
+_G.Scripts = {o = _G.ScriptsVersion,d = "discord.gg/KMQPWsSB",
 	[8425128940] = {
 		["Banana Hub"] = 
 		"https://scripts.luawl.com/hosted/3423/19726/AnimeWarriors2Free.lua"},
