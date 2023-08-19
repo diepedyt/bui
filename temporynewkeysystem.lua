846728cb6619a214bf2f6365f19cf100f1365a89
diepedyt <106111219+diepedyt@users.noreply.github.com>
2023-08-19 16:43:58 +0100

    Update temporynewkeysystem.lua

--- temporynewkeysystem.lua
+++ temporynewkeysystem.lua
@@ -1,5 +1,5 @@
 repeat task.wait() until game:IsLoaded()
-_G.ScriptsVersion = "yektseb"
+_G.ScriptsVersion = "yekpo"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/diepedyt/bui/main/initliasieryea.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/diepedyt/bui/main/mainBuiLoaderNew.lua"))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/diepedyt/bui/main/temporynewsystem.lua'))()
