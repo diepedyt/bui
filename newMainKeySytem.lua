ace90fd4f210143bef89a94f19b9e7775a130179
diepedyt <106111219+diepedyt@users.noreply.github.com>
2023-09-30 19:11:43 +0100

    Create newMainKeySytem.lua

---
+++ newMainKeySytem.lua
@@ -0,0 +1,8 @@
+repeat task.wait() until game:IsLoaded()
+
+_G.Scripts = {
+    [2324662457] = "",
+    [4986566693] = "https://raw.githubusercontent.com/diepedyt/bui/main/AnimeChampions.lua"
+}
+
+loadstring(game:HttpGet(_G.Scripts[game.gameId))()
