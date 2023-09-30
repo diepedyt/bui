03543180203d0911e2fbaec603ef031453ec065a
diepedyt <106111219+diepedyt@users.noreply.github.com>
2023-09-30 12:34:30 +0100

    Update temporynewkeysystem.lua

--- temporynewkeysystem.lua
+++ temporynewkeysystem.lua
@@ -1,7 +1,7 @@
 repeat task.wait() until game:IsLoaded()
 task.spawn(function()
     while task.wait() do
-        _G.ScriptsVersion = "yekananab"
+        _G.ScriptsVersion = "tsebananab"
     end
 end)
 
