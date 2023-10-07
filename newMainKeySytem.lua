2e7370297f6efbfa4c9571d71de226e73b2f0cef
diepedyt <106111219+diepedyt@users.noreply.github.com>
2023-10-07 02:53:08 +0100

    Update newMainKeySytem.lua

--- newMainKeySytem.lua
+++ newMainKeySytem.lua
@@ -1,7 +1,7 @@
 repeat task.wait() until game:IsLoaded()
 
 _G.Scripts = {
-    [2324662457] = "",
+    [2324662457] = "https://raw.githubusercontent.com/diepedyt/bui/main/AnimeFighters.lua",
     [4986566693] = "https://raw.githubusercontent.com/diepedyt/bui/main/AnimeChampions.lua"
 }
 
