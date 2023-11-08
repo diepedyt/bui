3c635958cebd46470ef31266f8e13c1967d9a103
diepedyt <106111219+diepedyt@users.noreply.github.com>
2023-11-08 20:07:13 +0000

    Update newMainKeySytem.lua

--- newMainKeySytem.lua
+++ newMainKeySytem.lua
@@ -3,7 +3,8 @@ repeat task.wait() until game:IsLoaded()
 _G.Scripts = {
     [2324662457] = "https://raw.githubusercontent.com/diepedyt/bui/main/AnimeFighters.lua",
     [4986566693] = "https://raw.githubusercontent.com/diepedyt/bui/main/AnimeChampions.lua",
-    [5166944221] = "https://raw.githubusercontent.com/diepedyt/bui/main/DeathBallLite.lua"
+    [5166944221] = "https://raw.githubusercontent.com/diepedyt/bui/main/DeathBallLite.lua",
+    [13772394625] = "https://raw.githubusercontent.com/diepedyt/bui/main/BladeBall.lua"
 }
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/diepedyt/bui/main/loader.lua"))()
