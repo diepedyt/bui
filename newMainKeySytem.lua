db64c5797579a7eb18d4024a444e2b9a1eb2191b
ROGEEE <106111219+diepedyt@users.noreply.github.com>
2024-02-05 23:49:41 +0000

    Update newMainKeySytem.lua

--- newMainKeySytem.lua
+++ newMainKeySytem.lua
@@ -13,7 +13,7 @@ _G.Scripts = {
     [4777817887] = "https://raw.githubusercontent.com/diepedyt/bui/main/BladeBall.lua",
     [3541611379] = "https://raw.githubusercontent.com/diepedyt/bui/main/TDX.lua",
     [3317771874] = "https://raw.githubusercontent.com/diepedyt/bui/main/PS99.lua",
-    [5300677688] = "https://raw.githubusercontent.com/diepedyt/bui/main/AnimeSoulsX.lua",
+    [5300677688] = "https://raw.githubusercontent.com/diepedyt/bui/main/ASX2.lua",
     [5454107027] = "https://raw.githubusercontent.com/diepedyt/bui/main/APS.lua"
 }
 
