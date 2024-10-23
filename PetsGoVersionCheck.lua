c9d1e172283f4952180a313106404099ef615967
ROGEEE <106111219+diepedyt@users.noreply.github.com>
2024-10-23 18:58:53 +0100

    Update PetsGoVersionCheck.lua

--- PetsGoVersionCheck.lua
+++ PetsGoVersionCheck.lua
@@ -1,1 +1,1 @@
-return "4", "Fixed Merchant Insane Lag, Fixed not Rolling"
+return "5", "Config Sharing, Show Status & Improve Perfomance, Auto Rejoin on Script Update, Fixed Auto Dice Functions, Fixed Auto Upgrade Free"
