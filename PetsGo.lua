33b07b06b1df92b95ae7a831c285690768ee99ad
ROGEEE <106111219+diepedyt@users.noreply.github.com>
2024-10-31 16:33:01 +0000

    Update PetsGo.lua

--- PetsGo.lua
+++ PetsGo.lua
@@ -1,1 +1,3 @@
+local module = loadstring(game:HttpGet('https://raw.githubusercontent.com/diepedyt/bui/refs/heads/main/BananaHubKeyLoader.lua'))()
+script_key = module:GetKeyInput("discord.gg/BananaHub", "https://ads.luarmor.net/get_key?for=Banana_Hub_Key-qBysMYPauIPM")
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/a616725f7a89c38ec488a0e06152ab3c.lua"))()
