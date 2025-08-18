3c5bcaf8fc729ef9d95aef3d9b93b382306d635e
ROGEEE <106111219+diepedyt@users.noreply.github.com>
2025-08-18 19:27:01 +0100

    Update BrainrotHub.lua

--- BrainrotHub.lua
+++ BrainrotHub.lua
@@ -1,3 +1,3 @@
 local module = loadstring(game:HttpGet('https://raw.githubusercontent.com/diepedyt/bui/refs/heads/main/BananaHubKeyLoader.lua'))()
-script_key = module:GetKeyInput("discord.gg/BananaHub", "https://ads.luarmor.net/get_key?for=Banana_Hub_Key_Pets_Go-DGYwAaLjBuvk")
+script_key = module:GetKeyInput("discord.gg/BananaHub", "https://ads.luarmor.net/v/cb/glivvlBfYmsc/HWeeXOWWampAhTPU")
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c9d1ce97d284685be9191fb8090e23c5.lua"))()
