8984c2417c2b2a31e460cf6e26032efb624d5af6
ROGEEE <106111219+diepedyt@users.noreply.github.com>
2025-08-18 19:43:03 +0100

    Update BrainrotHub.lua

--- BrainrotHub.lua
+++ BrainrotHub.lua
@@ -1,6 +1,7 @@
 local module = loadstring(game:HttpGet('https://raw.githubusercontent.com/diepedyt/bui/refs/heads/main/BananaHubKeyLoader.lua'))()
 local api = loadstring(game:HttpGet("https://sdkapi-public.luarmor.net/library.lua"))()
 script_key = module:GetKeyInput("guns.lol/BrainrotHub", "https://ads.luarmor.net/v/cb/glivvlBfYmsc/HWeeXOWWampAhTPU", function(key)
+  warn("attempting key", key)
   api.script_id = "c9d1ce97d284685be9191fb8090e23c5"
   local status = api.check_key(key)
   return status and status.code == "KEY_VALID"
