dfecc015083c059d9885875026b3399642ccc721
ROGEEE <106111219+diepedyt@users.noreply.github.com>
2025-08-23 23:47:14 +0100

    Update BananaHubLoader.lua

--- BananaHubLoader.lua
+++ BananaHubLoader.lua
@@ -1,1 +1,14 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/diepedyt/bui/main/temporynewkeysystem.lua"))()
+local api = loadstring(game:HttpGet("https://sdkapi-public.luarmor.net/library.lua"))()
+local module = loadstring(game:HttpGet('https://raw.githubusercontent.com/diepedyt/bui/refs/heads/main/BananaHubKeyLoader.lua'))()
+script_key = module:GetKeyInput("discord.gg/BananaHub", "https://ads.luarmor.net/get_key?for=Banana_Hub-hWOdrVMDQVrL", function(key)
+    api.script_id = "180154ec7abc246752b60603a7adc12d"
+    local status;
+    pcall(function()
+        status = api.check_key(key)
+        print(status.code)
+    end)
+    return status and status.code == "KEY_VALID"
+end)
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/180154ec7abc246752b60603a7adc12d.lua"))()
+
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/diepedyt/bui/main/temporynewkeysystem.lua"))()
