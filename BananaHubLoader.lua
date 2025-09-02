c05aa6ce14eb402374b061f0d8b138c15a0ac7e1
ROGEEE <106111219+diepedyt@users.noreply.github.com>
2025-09-02 22:16:31 +0100

    Update BananaHubLoader.lua

--- BananaHubLoader.lua
+++ BananaHubLoader.lua
@@ -1,29 +1,36 @@
 repeat task.wait() until game:IsLoaded()
 
---[[
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/180154ec7abc246752b60603a7adc12d.lua"))()
-]]
+if game.gameId == 7882829745 then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2f2cfcd821dc35ec9d51a13dd77e122e.lua"))()
+else
+    
+    --[[
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/180154ec7abc246752b60603a7adc12d.lua"))()
+    ]]
+    
+    local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/diepedyt/customLua/main/SimpleUiLib.lua"))()
+    ui.CreateBHUBPAID("")
+    
+    local vu = game:GetService("VirtualUser")
+    game:GetService("Players").LocalPlayer.Idled:connect(function()
+        vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+        task.wait(1)
+        vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+    end)
+    
+    task.spawn(function()
+        local VirtualInputManager = game:GetService("VirtualInputManager")
+        local X, Y = 111, 960
+        while task.wait() do
+            --VirtualInputManager:SendMouseButtonEvent(X, Y, 0, true, game, 1)
+            --VirtualInputManager:SendMouseButtonEvent(X, Y, 0, false, game, 1)
+            VirtualInputManager:SendKeyEvent(true, "Space", false, game)
+            task.wait(.2)
+            VirtualInputManager:SendKeyEvent(false, "Space", false, game)
+            task.wait(600)
+        end
+    end)
+    
+end
 
-local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/diepedyt/customLua/main/SimpleUiLib.lua"))()
-ui.CreateBHUBPAID("")
-
-local vu = game:GetService("VirtualUser")
-game:GetService("Players").LocalPlayer.Idled:connect(function()
-    vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-    task.wait(1)
-    vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-end)
-
-task.spawn(function()
-    local VirtualInputManager = game:GetService("VirtualInputManager")
-    local X, Y = 111, 960
-    while task.wait() do
-        --VirtualInputManager:SendMouseButtonEvent(X, Y, 0, true, game, 1)
-        --VirtualInputManager:SendMouseButtonEvent(X, Y, 0, false, game, 1)
-        VirtualInputManager:SendKeyEvent(true, "Space", false, game)
-        task.wait(.2)
-        VirtualInputManager:SendKeyEvent(false, "Space", false, game)
-        task.wait(600)
-    end
-end)
 
