84a75d6fbb677008a9ccba210dabc35d867a8106
ROGEEE <106111219+diepedyt@users.noreply.github.com>
2025-09-02 22:26:07 +0100

    Create BananaTestingVersion.lua

---
+++ BananaTestingVersion.lua
@@ -0,0 +1,7 @@
+repeat task.wait() until game:IsLoaded()
+
+-- testing branch, not on main script
+
+if game.gameId == 7882829745 then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2f2cfcd821dc35ec9d51a13dd77e122e.lua"))()
+end
