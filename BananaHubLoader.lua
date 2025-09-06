8d1e26f289d156386164f9440b2c324a6c536fff
ROGEEE <106111219+diepedyt@users.noreply.github.com>
2025-09-06 17:24:12 +0100

    Update BananaHubLoader.lua

--- BananaHubLoader.lua
+++ BananaHubLoader.lua
@@ -1,6 +1,6 @@
-repeat task.wait() until game:IsLoaded()
+repeat task.wait(.1) until game:IsLoaded()
 
-if game.gameId == 7882829745 then
+if  game.gameId == 7882829745 then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2f2cfcd821dc35ec9d51a13dd77e122e.lua"))()
 else
     
