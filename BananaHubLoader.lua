b9ba7ebc97842856c12af11326079335bb6f7e21
ROGEEE <106111219+diepedyt@users.noreply.github.com>
2025-09-18 23:01:23 +0100

    Update BananaHubLoader.lua

--- BananaHubLoader.lua
+++ BananaHubLoader.lua
@@ -1,7 +1,7 @@
 repeat task.wait(.1) until game:IsLoaded()
 
 if  game.gameId == 7882829745 then
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2f2cfcd821dc35ec9d51a13dd77e122e.lua"))()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/180154ec7abc246752b60603a7adc12d.lua"))()
 
     --[[
     local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/diepedyt/customLua/main/SimpleUiLib.lua"))()
