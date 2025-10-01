0d122436ca132695e39c7c8f3676dbb5bb21ccdb
ROGEEE <106111219+diepedyt@users.noreply.github.com>
2025-10-01 17:39:35 +0100

    Update BananaHubLoader.lua

--- BananaHubLoader.lua
+++ BananaHubLoader.lua
@@ -1,10 +1,9 @@
 repeat task.wait(.12345) until game:IsLoaded()
 
 if  game.gameId == 7882829745 then
-    --[[
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/180154ec7abc246752b60603a7adc12d.lua"))()
-    ]]
 
+    --[[
     local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/diepedyt/customLua/main/SimpleUiLib.lua"))()
     ui.CreateBHUBPAID("Anime Eternal")
     
@@ -27,6 +26,7 @@ if  game.gameId == 7882829745 then
             task.wait(600)
         end
     end)
+    ]]
     
 else
     
