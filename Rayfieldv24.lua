97ca675c88120ec6eb37ac5991cc71ea7b11d91d
ROGEEE <106111219+diepedyt@users.noreply.github.com>
2025-11-13 09:28:02 +0000

    Update Rayfieldv24.lua

--- Rayfieldv24.lua
+++ Rayfieldv24.lua
@@ -1,5 +1,5 @@
 
-print("v24 v2")
+print("v24 v3")
 --[[
 
 Rayfield Interface Suite
@@ -375,6 +375,7 @@ local Topbar = Main.Topbar
 local TabListBack = Main.TabListBack
 local Elements = Main.Elements
 local PremiumTag = Main.Premium
+PremiumTag.BackgroundTransparency = 0.9
 local LoadingFrame = Main.LoadingFrame
 local TabList = Main.TabList
 local SearchBar = Main.Searchbar
