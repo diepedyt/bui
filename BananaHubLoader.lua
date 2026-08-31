34139586b7d4859562c2565f5477b050a4ac34ff
ROGEEE <106111219+diepedyt@users.noreply.github.com>
2026-08-31 15:21:10 +0100

    Update BananaHubLoader.lua

--- BananaHubLoader.lua
+++ BananaHubLoader.lua
@@ -15,7 +15,7 @@ end)
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/180154ec7abc246752b60603a7adc12d.lua"))()
 ]]
 
-
+--[[
 local module = loadstring(game:HttpGet("https://raw.githubusercontent.com/diepedyt/bui/refs/heads/main/BananaHubKeyLoaderV2.lua"))()
 module:SetSavedKeyFile("BananaHub"..game.gameId..game.Players.LocalPlayer.UserId)
 script_key = module:GetKeyInput("discord.gg/BananaHub", "https://raw.githubusercontent.com/diepedyt/bui/refs/heads/main/HowToFreeKey.txt", function(key)
@@ -24,13 +24,13 @@ script_key = module:GetKeyInput("discord.gg/BananaHub", "https://raw.githubuserc
 	return key == "banana"
 end)
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/180154ec7abc246752b60603a7adc12d.lua"))()
+]]
 
 
 
---[[
 local simpleLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/diepedyt/bui/refs/heads/main/SimpleUILibV2.lua"))()
 simpleLib.CreateBHUBPAID()
-]]
+
 
 local vu = game:GetService("VirtualUser")
 game:GetService("Players").LocalPlayer.Idled:connect(function()
