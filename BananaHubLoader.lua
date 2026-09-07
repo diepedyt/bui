6e99b9119766f79549b8a05c90f6cd8aeb7d7821
ROGEEE <106111219+diepedyt@users.noreply.github.com>
2026-09-07 22:08:21 +0100

    Update BananaHubLoader.lua

--- BananaHubLoader.lua
+++ BananaHubLoader.lua
@@ -23,14 +23,14 @@ script_key = module:GetKeyInput("discord.gg/BananaHub", "https://raw.githubuserc
 	key = key:lower()
 	return key == "banana"
 end)
-]]
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/180154ec7abc246752b60603a7adc12d.lua"))()
+]]
 
 
---[[
+
 local simpleLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/diepedyt/bui/refs/heads/main/SimpleUILibV2.lua"))()
 simpleLib.CreateBHUBPAID()
-]]
+
 
 local vu = game:GetService("VirtualUser")
 game:GetService("Players").LocalPlayer.Idled:connect(function()
